ef2eee339427c0b05673f7b8c77db889990fc16f
EvertonKauan <everton.kauan.silva.oliveira@ccc.ufcg.edu.br>
2023-05-27 12:40:34 -0300

    Differential Privacy: Padrão VCV nos nomes, considera espaços, idade > 18 e sensibilidade 5

--- main.py
+++ main.py
@@ -3,21 +3,30 @@ import numpy
 
 # Função para gerar nomes privados com a mesma quantidade de letras
 def gera_nome_anonimo(nome):
-    letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    vogais = "AEIOU"
+    consoantes = "BCDFGHJKLMNPQRSTVWXYZ"
     nome_privado = ''
-    for i in range(0, len(nome)):
-        nome_privado += ''.join(random.choice(letras))
+    for i in range(len(nome)):
+        if nome[i].isspace():
+            nome_privado += ' '
+        elif nome[i].isalpha():
+            if i % 2 == 0:
+                nome_privado += random.choice(vogais)
+            else:
+                nome_privado += random.choice(consoantes)
     return nome_privado
-
 
 # Função para gerar idades privadas
 def gera_idade_anonima(idade):
     epsilon = 1.0 # Controlador de nível de privacidade (1 geralmente é considerado como um nível moderado de privacidade)
-    sensibilidade = 1.0 # Sensibilidade igual a 1 para valores não duplicados, sensibilidade padrão
+    sensibilidade = 2.0 # Sensibilidade igual a 1 para valores não duplicados, sensibilidade padrão
     ruido = numpy.random.laplace(scale=sensibilidade/epsilon) # Distribuição de laplace é usada comumente em DP
     idade_ruidosa = idade + ruido
+    
+    if idade_ruidosa < 18:
+        idade_ruidosa = gera_idade_anonima(idade)
+        
     return idade_ruidosa
-
 
 # Função para gerar salários privados
 def gera_salario_anonimo(valor):
@@ -50,5 +59,5 @@ for i in range(0, len(dados_pessoais)):
 for i in range(0, len(dados_pessoais)):
     print('-=' * 40)
     print(f"| Nome original =   {dados_pessoais[i]['nome']:>10} | Nome privado = {dados_anonimos[i]['nome']:>10}")
-    print(f"| Idade original =  {dados_pessoais[i]['idade']:>10} | Idade privada = {dados_anonimos[i]['idade']:<10.0f}")
+    print(f"| Idade original =  {dados_pessoais[i]['idade']:>10} | Idade privada = {dados_anonimos[i]['idade']:>10.0f}")
     print(f"| Salário original ={dados_pessoais[i]['salario']:>10} | Salário privado = {dados_anonimos[i]['salario']:>10.2f}")
