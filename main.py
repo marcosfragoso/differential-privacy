b787a8bbec6f00b8b8cf796fd4abf0aef16ea7fa
Marcos <fragoso.ufcg@gmail.com>
2023-05-31 15:17:53 -0300

    altera funcoes e add medias

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 from diffprivlib.mechanisms import Laplace
-import numpy as np
+
 
 def adicionar_ruido_idade(valor, sensibilidade, epsilon):
-    while True: #Faz com que a idade esteja dentro da sensibilidade
-    	ruido = Laplace(epsilon=epsilon, sensitivity=sensibilidade).randomise(0)  # Adiciona ruído de Laplace
-    	valor_privado = valor + ruido
-    	if valor_privado < valor + sensibilidade_idade and valor_privado > sensibilidade_idade - valor:
-    	    return valor_privado
+    valor_privado = Laplace(epsilon=epsilon, sensitivity=sensibilidade).randomise(valor)  # Adiciona ruído de Laplace
+    return valor_privado
     
 
 def adicionar_ruido_salario(valor, sensibilidade, epsilon):
-    while True: #Faz com que o salário esteja dentro da sensibilidade
-    	ruido = Laplace(epsilon=epsilon, sensitivity=sensibilidade).randomise(0)  # Adiciona ruído de Laplace
-    	valor_privado = valor + ruido
-    	if valor_privado < valor + sensibilidade_salario and valor_privado > sensibilidade_salario - valor:
-    	    return valor_privado
+    valor_privado = Laplace(epsilon=epsilon, sensitivity=sensibilidade).randomise(valor)  # Adiciona ruído de Laplace
+    return valor_privado
+
 
 dados_pessoais = [
     {'salario': 3500.34,'idade': 23}, 
@@ -30,17 +25,27 @@ dados_pessoais = [
     {'salario': 6400.00,'idade': 21}, 
 ]
 
-sensibilidade_salario = 500 #O salário sempre estará dentro desse valor, maior ou menor
+sensibilidade_salario = 1000 
 epsilon_salario = 1.0
 
-sensibilidade_idade = 5 #A idade sempre estará dentro desse valor, maior ou menor
+sensibilidade_idade = 2 
 epsilon_idade = 1.0
 
 dados_anonimos = []
 
+soma_salarios_privados = 0
+soma_salarios_originais = 0
+soma_idades_privadas = 0
+soma_idades_originais = 0
+
 for dado in dados_pessoais:
     salario_privado = adicionar_ruido_salario(dado['salario'], sensibilidade_salario, epsilon_salario)
     idade_privada = adicionar_ruido_idade(dado['idade'], sensibilidade_idade, epsilon_idade)
+
+    soma_salarios_originais += dado['salario']
+    soma_salarios_privados += salario_privado
+    soma_idades_originais += dado['idade']
+    soma_idades_privadas += idade_privada
 
     dados_anonimos.append({'salario': salario_privado, 'idade': idade_privada})
 
@@ -49,3 +54,14 @@ for i in range(len(dados_pessoais)):
     print(f"| Idade original =  {dados_pessoais[i]['idade']:>10} | Idade privada = {dados_anonimos[i]['idade']:>10.0f}")
     print(f"| Salário original ={dados_pessoais[i]['salario']:>10} | Salário privado = {dados_anonimos[i]['salario']:>10.2f}")
 
+print('-=' * 40)
+print(f'| Média idades originais =   {soma_idades_originais/len(dados_pessoais):>10.2f} | Média idades privadas = {soma_idades_privadas/len(dados_pessoais):>10.2f}')
+print(f'| Média salários originais = {soma_salarios_originais/len(dados_pessoais):>10.2f} | Média salários privados = {soma_salarios_privados/len(dados_pessoais):>10.2f}')
+
+#       Conclusão: Podemos observar a importância da escolha do ruído para cada atributo: 
+# i)    Para a idade, por ser menor o ruído, a média privada se aproxima mais da média original.
+# ii)   Para o salário, por ser maior o ruído, a média privada se afasta mais da média original.
+# iii)  Para ambos os atributos, a média privada se aproxima da média original conforme aumentamos o número de dados.
+# iv)   Para ambos os atributos, a média privada se aproxima da média original conforme diminuimos o ruído.
+# v)    Para ambos os atributos, a média privada se afasta da média original conforme aumentamos o ruído.
+# vi)   Para ambos os atributos, a média privada se afasta da média original conforme diminuimos o número de dados.
